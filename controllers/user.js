ad5ccece7948bd3d9be53bc7382ce19d37a89ba5
Урусова Светлана <77745487+jazzyvesper@users.noreply.github.com>
2022-02-15 22:29:29 +0300

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -43,7 +43,7 @@ const login = (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
         sameSite: 'none',
-        //secure: true,
+        secure: true,
       })
       .send({ 
         succes: 'ok',
